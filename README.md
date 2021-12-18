fcc067e460bfecf74486d02f3bc531f86ce08c1e
Kimiaki Kuno <knokmki612@gmail.com>
2021-12-18 17:00:18 +0900

    feat: demonstrate did

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-# TypeScript Boilerplate
+# Sandbox of DID(:web)(:jwt)(:jwt-vc)
 
 - No specifying directory hierarchy
 - CI (Lint / Test / Format) ready
 - Library update automation ready
 - Configure with recommended presets as possible
+
+## Usage
+
+```shell
+$ ssh-keygen -t ed25519 -f id_ed25519
+$ echo "PRIVATE_KEY=$(cat ./id_ed25519 | head -n -1 | tail -n +2 | tr -d '\n')" >> .env
+```
+
+## Related Docs
+
+- [did:web Method Specification](https://w3c-ccg.github.io/did-method-web/)
+- [Ed25519 Signature 2020](https://w3c-ccg.github.io/lds-ed25519-2020/)
+- [Web DID Resolver](https://github.com/decentralized-identity/web-did-resolver/blob/master/README.md)
+- [did-jwt](https://github.com/decentralized-identity/did-jwt/blob/master/README.md)
+- [did-jwt-vc](https://github.com/decentralized-identity/did-jwt-vc/blob/master/README.md)
---
+++ index.ts
@@ -0,0 +1,57 @@
+import { EdDSASigner } from "did-jwt";
+import { Resolver } from "did-resolver";
+import { getResolver } from "web-did-resolver";
+import {
+  Issuer,
+  JwtCredentialPayload,
+  createVerifiableCredentialJwt,
+  JwtPresentationPayload,
+  createVerifiablePresentationJwt,
+  verifyCredential,
+  verifyPresentation,
+} from "did-jwt-vc";
+
+/** Prerequisites of Creating JWTs */
+const issuer = {
+  did: 'did:web:knokmki612.github.io:sandbox-did-web-jwt-vc',
+  signer: EdDSASigner(process.env.PRIVATE_KEY ?? ""),
+  alg: 'EdDSA',
+} as Issuer;
+
+/** Prerequisites of Veryfying JWTs */
+const resolver = new Resolver(getResolver());
+
+async function main() {
+  /** Create a Verifiable Credential */
+  const vcPayload: JwtCredentialPayload = {
+    sub: "",
+    nbf: NaN,
+    vc: {
+      "@context": ["https://www.w3.org/2018/credentials/v1"],
+      type: ["VerifiableCredential"],
+      credentialSubject: {
+        hoge: {
+          fuga: "hogehoge",
+        },
+      },
+    },
+  };
+  const vcJwt = await createVerifiableCredentialJwt(vcPayload, issuer);
+  /** Create a Verifiable Presentation */
+  const vpPayload: JwtPresentationPayload = {
+    vp: {
+      "@context": ["https://www.w3.org/2018/credentials/v1"],
+      type: ["VerifiablePresentation"],
+      verifiableCredential: [vcJwt],
+    },
+  };
+  const vpJwt = await createVerifiablePresentationJwt(vpPayload, issuer);
+  /** Verifying a Verifiable Credential */
+  const verifiedVC = await verifyCredential(vcJwt, resolver);
+  console.log(verifiedVC)
+  /** Veryfing a Verifiable Presentation */
+  const verifiedVP = await verifyPresentation(vpJwt, resolver);
+  console.log(verifiedVP)
+}
+
+export default main;
